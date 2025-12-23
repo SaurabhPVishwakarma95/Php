a48c1f2e145cfe4713ab4c8563da0ed7e2e12330
SaurabhPVishwakarma95 <saurabhpv1995@gmail.com>
2025-12-23 18:11:44 +0530

    Restore request method handling in index.php
    Reintroduced request method handling for API responses.

--- index.php
+++ index.php
@@ -4,21 +4,18 @@
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
     $origin_data = json_decode(file_get_contents("origin_data.json"), true);
 
-    echo "<h1>API Data</h1>";
+    echo "<h1>API Data</h1>";    
 
-    echo json_encode($origin_data);
-
-    // $method_action = $_SERVER["REQUEST_METHOD"];
-
-    // switch($method_action) {
-    //     case "GET":
-    //         echo json_encode($origin_data);
-    //         break;
-    //     default:
-    //         echo json_encode(["exception" => "Found Exception on Requesting API"]);
-    // }
-
+    $method_action = $_SERVER["REQUEST_METHOD"];
+    switch($method_action) {
+        case "GET":
+            echo json_encode($origin_data);
+            break;
+        default:
+            echo json_encode(["exception" => "Found Exception on Requesting API"]);
+    }
 ?>
+
 
 
 
