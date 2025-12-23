89503cb30a1fc05fa20e3cad1b42b640807b4bc8
SaurabhPVishwakarma95 <saurabhpv1995@gmail.com>
2025-12-23 18:04:11 +0530

    Add files via upload

---
+++ api/index.php
@@ -0,0 +1,16 @@
+<?php
+    header("Content-Type, application/json");
+    header("Access-Control-Allow-Origin, *");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
+    $origin_data = json_decode(file_get_contents("../origin_data.json"), true);
+    $method_action = $_SERVER["REQUEST_METHOD"];
+
+    switch($method_action) {
+        case "GET":
+            echo json_encode($origin_data);
+            break;
+        default:
+            echo json_encode(["exception" => "Found Exception on Requesting API"]);
+    }
+
+?>
