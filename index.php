946e7ceeaf72c36112996380d8e36ba92aeebe5e
SaurabhPVishwakarma95 <saurabhpv1995@gmail.com>
2025-12-23 17:56:12 +0530

    Add HTML header to JSON API response
    Added an HTML header to the API response.

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
     $origin_data = json_decode(file_get_contents("origin_data.json"), true);
 
+    echo "<h1>My PHP API</h1>";
+
     echo json_encode($origin_data);
 
     // $method_action = $_SERVER["REQUEST_METHOD"];
@@ -17,4 +19,5 @@
     // }
 
 ?>
+
 
